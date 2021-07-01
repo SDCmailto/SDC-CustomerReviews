3d7e49a3db734900e6b4dcb2d841f630ac863f01
Hannah Manfredi <hmanfredi@gmail.com>
2021-06-30 20:03:35 -0700

    Set up for generating a csv file for each table

--- database/postgres/csv.js
+++ database/postgres/csv.js
@@ -2,10 +2,36 @@
 const fs = require('fs');
 const utils = require('./utils');
 const fastcsv = require('fast-csv');
-const ws = fs.createWriteStream("products.csv");
+
+const ws_product = fs.createWriteStream("products.csv");
+const ws_reviews = fs.createWriteStream("reviews.csv");
+const ws_users  = fs.createWriteStream("users.csv");
+const ws_features  = fs.createWriteStream("features.csv");
+const ws_productFeatures = fs.createWriteStream("productFeatures.csv");
 
 let products = utils.createProducts()
+let reviews = utils.createReviews()
+let users = utils.createUsers()
+let features = utils.createFeatures()
+let productFeatures = utils.createProductFeatures()
 
 fastcsv
   .write(products, { headers: true })
-  .pipe(ws);
+  .pipe(ws_product);
+
+fastcsv
+.write(reviews , { headers: true })
+.pipe(ws_reviews);
+
+fastcsv
+.write(reviews , { headers: true })
+.pipe(ws_users);
+
+fastcsv
+.write(reviews , { headers: true })
+.pipe(ws_features);
+
+fastcsv
+.write(reviews , { headers: true })
+.pipe(ws_productFeatures);
+
