f31ade87621d71f3eadac3ec4b50c542381fb2c6
Hannah Manfredi <hmanfredi@gmail.com>
2021-08-10 20:35:40 -0700

    update seeding script

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require("./server.js");
 const PORT = 3004;
 
 app.listen(PORT, ()=>{
-  console.log(`Server now listening at http://52.14.110.72:3004/${PORT}`)
+  console.log(`Server now listening at http://52.14.110.72:${PORT}`)
 })
