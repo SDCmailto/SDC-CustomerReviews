106a1f57325470e7cea506e4fc6e0c8f0571074c
Hannah Manfredi <hmanfredi@gmail.com>
2021-08-11 13:27:56 -0700

    update service ec2 ip

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require("./server.js");
 const PORT = 3004;
 
 app.listen(PORT, ()=>{
-  console.log(`Server now listening at http://52.14.110.72:${PORT}`)
+  console.log(`Server now listening at http://3.16.203.185:${PORT}`)
 })
