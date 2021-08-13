6b9f0d298d13add38c63680ad998bbaf18d063cd
Hannah Manfredi <hmanfredi@gmail.com>
2021-08-12 18:39:40 -0700

    Update ip addresses

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require("./server.js");
 const PORT = 3004;
 
 app.listen(PORT, ()=>{
-  console.log(`Server now listening at http://3.16.203.185:${PORT}`)
+  console.log(`Server now listening at http://18.221.116.138:${PORT}`)
 })
