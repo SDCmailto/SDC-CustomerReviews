2851486ee07a5efa3fc8481bae8d4f3862400b33
Hannah Manfredi <hmanfredi@gmail.com>
2021-08-13 11:43:17 -0700

    update seed

--- database/postgres/seed2.js
+++ database/postgres/seed2.js
@@ -7,7 +7,7 @@ const seed = () => {
     let date = JSON.stringify(faker.date.past()).slice(1, 11);
     let review = {
         id: i,
-        title: faker.lorem.words().replace(/,/g, "").slice(0, 2),
+        // title: faker.lorem.words().replace(/,/g, "").slice(0, 2),
         abuseReported: faker.datatype.boolean(),
         rating: faker.datatype.number(5),
         location_: faker.address.country().replace(/,/g, "").slice(0, 2),
@@ -29,7 +29,7 @@ const seed = () => {
       // helpfulCount: faker.datatype.number(2000)
     // }
 
-    let q = `INSERT INTO r(id, title, abuseReported, rating, location_, userid, productid, reviewDate, reviewBody, helpfulCount) VALUES(${review.id}, ${review.title}, ${review.abuseReported}, ${review.rating}, ${review.location_}, ${review.userid}, ${review.productid}, ${review.reviewDate}, ${review.reviewBody}, ${review.helpfulCount})`;
+    let q = `INSERT INTO r(id, abuseReported, rating, location_, userid, productid, reviewDate, reviewBody, helpfulCount) VALUES(${review.id}, ${review.abuseReported}, ${review.rating}, ${review.location_}, ${review.userid}, ${review.productid}, ${review.reviewDate}, ${review.reviewBody}, ${review.helpfulCount})`;
 
     // let q = `INSERT INTO reviews(productid) VALUES (${review.productid})`;
 
