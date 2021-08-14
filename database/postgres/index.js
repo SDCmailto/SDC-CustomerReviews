365745ed9d599ecda1ffbcfbaa11009a42fcef71
Hannah Manfredi <hmanfredi@gmail.com>
2021-08-14 11:35:07 -0700

    uncomment db methods

--- database/postgres/index.js
+++ database/postgres/index.js
@@ -15,103 +15,103 @@ client
   .then(() => console.log('connected'))
   .catch(err => console.error('connection error', err.stack))
 
-// const findAllReviews = async (productId) => {
-//   const query = {
-//     text: `SELECT * FROM reviews JOIN products ON (reviews.productid = products.id) WHERE productid = $1`,
-//     values: [productId]
-//   };
-//   let data = [];
-//   await client.query(query)
-//     .then(res => {
-//       data.push(res.rows)
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-//   return data;
-// };
+const findAllReviews = async (productId) => {
+  const query = {
+    text: `SELECT * FROM reviews JOIN products ON (reviews.productid = products.id) WHERE productid = $1`,
+    values: [productId]
+  };
+  let data = [];
+  await client.query(query)
+    .then(res => {
+      data.push(res.rows)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return data;
+};
 
-// const findAvgRating = async (productid) => {
-//   const query = {
-//     text: 'SELECT avgRating FROM products WHERE id = $1',
-//     values: [productid]
-//   };
-//   let data = [];
-//   await client.query(query)
-//     .then(res => {
-//       data.push(res.rows)
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-//   return data[0];
-// }
+const findAvgRating = async (productid) => {
+  const query = {
+    text: 'SELECT avgRating FROM products WHERE id = $1',
+    values: [productid]
+  };
+  let data = [];
+  await client.query(query)
+    .then(res => {
+      data.push(res.rows)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return data[0];
+}
 
-// const createReview = async (productid, review) => {
-//   console.log('review: ', review)
-//   let numbProductId = parseInt(review.productid);
-//   const idQuery = {
-//     text: `SELECT COUNT (*) FROM reviews`,
-//   };
-//   const result = await client.query({
-//     rowMode: 'array',
-//     text: 'SELECT COUNT (*) FROM reviews;',
-//   })
-//   let currentReviewId = result.rows[0][0]
-//   console.log('currentReviewId : ', currentReviewId)
-//   let id = parseInt(currentReviewId);
-//   let userId = 0;
-//   const userIdResult = await client.query({
-//     rowMode: 'array',
-//     text: `SELECT * FROM users WHERE name_ = $1`,
-//     values: [review.username]
-//   })
-//   if (!userIdResult.rowCount) {
-//     const makeUserId = client.query({
-//       rowMode: 'array',
-//       text: `SELECT COUNT (*) FROM users;`
-//     })
-//     userId = parseInt((result.rows[0][0]) + 1)
-//     let userrating = faker.datatype.number(15000);
-//     let totalreviews = faker.datatype.number(10000);
-//     const createUserQuery =
-//     await client.query({
-//       text: `INSERT INTO users VALUES ($1, $2, $3, $4)`,
-//       values: [userId, review.username, userrating, totalreviews]
-//     });
-//   } else {
-//     userId = parseInt(userIdResult.rows[0][0])
-//   }
-//   let newReviewData = [];
-//   const data = await client.query(query = {
-//     rowMode: 'array',
-//     text: `INSERT INTO reviews VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`,
-//     values: [id, review.title, review.abuseReported, review.rating, review.location_, userId, numbProductId, review.reviewDate, review.reviewBody, review.helpfulCount]
-//   });
-//   return data.rows[0];
-// }
+const createReview = async (productid, review) => {
+  console.log('review: ', review)
+  let numbProductId = parseInt(review.productid);
+  const idQuery = {
+    text: `SELECT COUNT (*) FROM reviews`,
+  };
+  const result = await client.query({
+    rowMode: 'array',
+    text: 'SELECT COUNT (*) FROM reviews;',
+  })
+  let currentReviewId = result.rows[0][0]
+  console.log('currentReviewId : ', currentReviewId)
+  let id = parseInt(currentReviewId);
+  let userId = 0;
+  const userIdResult = await client.query({
+    rowMode: 'array',
+    text: `SELECT * FROM users WHERE name_ = $1`,
+    values: [review.username]
+  })
+  if (!userIdResult.rowCount) {
+    const makeUserId = client.query({
+      rowMode: 'array',
+      text: `SELECT COUNT (*) FROM users;`
+    })
+    userId = parseInt((result.rows[0][0]) + 1)
+    let userrating = faker.datatype.number(15000);
+    let totalreviews = faker.datatype.number(10000);
+    const createUserQuery =
+    await client.query({
+      text: `INSERT INTO users VALUES ($1, $2, $3, $4)`,
+      values: [userId, review.username, userrating, totalreviews]
+    });
+  } else {
+    userId = parseInt(userIdResult.rows[0][0])
+  }
+  let newReviewData = [];
+  const data = await client.query(query = {
+    rowMode: 'array',
+    text: `INSERT INTO reviews VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`,
+    values: [id, review.title, review.abuseReported, review.rating, review.location_, userId, numbProductId, review.reviewDate, review.reviewBody, review.helpfulCount]
+  });
+  return data.rows[0];
+}
 
-// const updateReview = async (productid, review) => {
-//   console.log('in db update review')
-//   const task = await client.query(query = {
-//     rowMode: 'array',
-//     text: `UPDATE reviews SET title = $1 WHERE title = 'a';`,
-//     values: ['the']
-//   });
-//   console.log('task: ', task);
-//   return `Review ${review} successfully updated.`;
-// }
+const updateReview = async (productid, review) => {
+  console.log('in db update review')
+  const task = await client.query(query = {
+    rowMode: 'array',
+    text: `UPDATE reviews SET title = $1 WHERE title = 'a';`,
+    values: ['the']
+  });
+  console.log('task: ', task);
+  return `Review ${review} successfully updated.`;
+}
 
-// const deleteReview = async (productid, review) => {
-//   console.log('in db delete review')
-//   const task = await client.query(query = {
-//     rowMode: 'array',
-//     text: `DELETE FROM reviews WHERE id = $1;`,
-//     values: [review]
-//   });
-//   console.log('task: ', task);
-//   return `Review ${review} successfully deleted.`;
-// }
+const deleteReview = async (productid, review) => {
+  console.log('in db delete review')
+  const task = await client.query(query = {
+    rowMode: 'array',
+    text: `DELETE FROM reviews WHERE id = $1;`,
+    values: [review]
+  });
+  console.log('task: ', task);
+  return `Review ${review} successfully deleted.`;
+}
 
 // const seed = () => {
 
@@ -150,11 +150,11 @@ client
 
 // // benchmark();
 // module.exports.seed = seed;
-// module.exports.findAllReviews = findAllReviews;
-// module.exports.findAvgRating = findAvgRating;
-// module.exports.createReview = createReview;
-// module.exports.updateReview = updateReview;
-// module.exports.deleteReview = deleteReview;
+module.exports.findAllReviews = findAllReviews;
+module.exports.findAvgRating = findAvgRating;
+module.exports.createReview = createReview;
+module.exports.updateReview = updateReview;
+module.exports.deleteReview = deleteReview;
 
 module.exports.client = client;
 
